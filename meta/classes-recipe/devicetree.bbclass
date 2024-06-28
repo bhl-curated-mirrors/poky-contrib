openembedded/openembedded-core

commit 7a06069a041a908c1d190f8fb7dd923b86217703

devicetree.bbclass: switch away from S = WORKDIR
Since the change done in commit 32cba1cc916a ("insane: Error for S ==
WORKDIR"), usage of WORKDIR is not allowed anymore.

Switch S from WORKDIR to UNPACKDIR as implemented in commit d9328e3b0b06
("recipes: Switch away from S = WORKDIR").

Signed-off-by: Andrey Zhizhikin <andrey.z@gmail.com>
Signed-off-by: Alexandre Belloni <alexandre.belloni@bootlin.com>
Signed-off-by: Richard Purdie <richard.purdie@linuxfoundation.org>

meta/classes-recipe/devicetree.bbclass
@@ -40,7 +40,8 @@ PACKAGE_ARCH = "${MACHINE_ARCH}"
 SYSROOT_DIRS += "/boot/devicetree"
 FILES:${PN} = "/boot/devicetree/*.dtb /boot/devicetree/*.dtbo"
 
-S = "${WORKDIR}"
+S = "${WORKDIR}/sources"
+UNPACKDIR = "${S}"
 B = "${WORKDIR}/build"
 
 # Default kernel includes, these represent what are normally used for in-kernel

bhl-curated-mirrors/poky-contrib

commit 97fcffbaf6603e2d88d448439607a26918769e27

devicetree.bbclass: fix setting S
When a user of devicetree.bbclass overrides setting S, e.g. to
git/overlays then having UNPACKDIR set to S results in that
git repo unpacked inside UNPACKDIR. Thus the sources are actually
in git/overlays/git/overlays rather than the expected git/overlays.

Change the logic, so that S by default is set to UNPACKDIR,
but a user can change S to point to were the sources actually
will be.

Fixes: 7a06069a041a ("devicetree.bbclass: switch away from S = WORKDIR")
(From OE-Core rev: d604229fcbf47642d3c2b3480c0ef548d120d3cf)

Signed-off-by: Max Krummenacher <max.krummenacher@toradex.com>
Signed-off-by: Alexandre Belloni <alexandre.belloni@bootlin.com>

meta/classes-recipe/devicetree.bbclass
@@ -40,8 +40,8 @@ PACKAGE_ARCH = "${MACHINE_ARCH}"
 SYSROOT_DIRS += "/boot/devicetree"
 FILES:${PN} = "/boot/devicetree/*.dtb /boot/devicetree/*.dtbo"
 
-S = "${WORKDIR}/sources"
-UNPACKDIR = "${S}"
+UNPACKDIR = "${WORKDIR}/sources"
+S = "${UNPACKDIR}"
 B = "${WORKDIR}/build"
 
 # Default kernel includes, these represent what are normally used for in-kernel